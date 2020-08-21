252142b34df55e3c58738fa1076267ce17d8324d
imsoncod <iustron@naver.com>
2020-08-21 18:28:24 +0900

    [Add] 5차 1급 solution_code

---
+++ 5차/5차 1급 6_solution_code.py
@@ -0,0 +1,35 @@
+#다음과 같이 import를 사용할 수 있습니다.
+#import math
+
+def solution(s1, s2, p, q):
+    #여기에 코드를 작성해주세요.
+    answer = ''
+    num1 = 0
+    num2 = 0
+    n = len(s1)
+    m = len(s2)
+    
+    for i in range(n):
+        num1 += int(s1[i])*(p**(n-i-1))
+        print(s1[i], p**(n-i-1))
+    for i in range(m):
+        num2 += int(s2[i])*(p**(m-i-1))
+        
+    num = num1 + num2
+    print(num1, num2)
+    
+    while num != 0:
+        answer += str(num%q)
+        num //= q
+        
+    return answer[::-1]
+
+#아래는 테스트케이스 출력을 해보기 위한 코드입니다.
+s1 = "112001"
+s2 = "12010"
+p = 3
+q = 8
+ret = solution(s1, s2, p, q)
+
+#[실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+print("solution 함수의 반환 값은", ret, "입니다.")
